870366cc4f5d84b6c450f46e8cf68a68d4c5c424
Chris For Rod Of Jesse <77516578+Rodofjesse@users.noreply.github.com>
2022-10-28 05:05:11 +0100

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,25 @@
+package com.example.recyclerviewscodepath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+    }
+}
