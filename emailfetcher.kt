642c03a9b6832888496c24e4025eaaf3ed871d08
Chris For Rod Of Jesse <77516578+Rodofjesse@users.noreply.github.com>
2022-10-28 05:06:54 +0100

    Create emailfetcher.kt

---
+++ emailfetcher.kt
@@ -0,0 +1,24 @@
+class EmailFetcher {
+    companion object {
+        val senders = listOf("Christopher Uloko", "Dahlia Cline", "Kevin Miranda", "Kaya Austin", "Laila Calderon", "Marquise Rhodes", "Fletcher Patel", "Luz Barron", "Kamren Dudley", "Jairo Foster", "Lilah Sandoval", "Ansley Blake", "Slade Sawyer", "Jaelyn Holmes", "Phoenix Bright", "Ernesto Gould")
+        val title = "Welcome to Kotlin!"
+        val summary = "Welcome to the Android Kotlin Course! We're excited to have you join us and learn how to develop Android apps using Kotlin. Here are some tips to get started."
+        fun getEmails(): MutableList<Email> {
+            var emails : MutableList<Email> = ArrayList()
+            for (i in 0..9) {
+                val email = Email(senders[i], title, summary)
+                emails.add(email)
+            }
+            return emails
+        }
+
+        fun getNext5Emails(): MutableList<Email> {
+            var newEmails : MutableList<Email> = ArrayList()
+            for (i in 10..14) {
+                val email = Email(senders[i], title, summary)
+                newEmails.add(email)
+            }
+            return newEmails
+        }
+    }
+}
